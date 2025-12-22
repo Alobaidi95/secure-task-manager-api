9937b2d1037eadca0b4ddd856a63f4db02a47d82
osamah <osamaaliraqi95@gmail.com>
2025-12-22 01:38:31 -0600

    Add UserService with secure user registration

---
+++ src/main/java/com/osamah/taskmanager/service/UserService.java
@@ -0,0 +1,7 @@
+package com.osamah.taskmanager.service;
+
+import com.osamah.taskmanager.model.User;
+public interface UserService {
+
+    User registerUser(String username, String email, String rawPassword);
+}
---
+++ src/main/java/com/osamah/taskmanager/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.osamah.taskmanager.service.impl;
+
+import com.osamah.taskmanager.model.Role;
+import com.osamah.taskmanager.model.User;
+import com.osamah.taskmanager.repository.UserRepository;
+import com.osamah.taskmanager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(String username, String email, String rawPassword) {
+        if(userRepository.existsByEmail(email))
+        {
+            throw new RuntimeException("Email already exists");
+        }
+        if(userRepository.existsByUsername(username))
+        {
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = User.builder()
+                .username(username)
+                .email(email)
+                .password(passwordEncoder.encode(rawPassword))
+                .role(Role.USER)
+                .build();
+
+        return userRepository.save(user);
+
+
+    }
+}
